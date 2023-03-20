7e11df00d5f3b79bd9bd6917c6e5abdf6dffad35
ThiliniLFernando <fthilini34@gmail.com>
2023-03-20 12:04:57 +0530

    Initial commit

---
+++ src/main/java/com/thilini/solutiondriven/solutiondrivenproject/SolutionDrivenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thilini.solutiondriven.solutiondrivenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionDrivenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionDrivenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thilini/solutiondriven/solutiondrivenproject/SolutionDrivenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilini.solutiondriven.solutiondrivenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionDrivenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
